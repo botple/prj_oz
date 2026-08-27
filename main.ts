1c827d9d7a4ea3bc2e98110db1e58cb2fecea40b
botple <haminseung1st@gmail.com>
2026-08-28 01:39:19 +0900

    main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 DFRobotMaqueenPlus.I2CInit()
+
 basic.forever(function () {
-    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 100)
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 100)
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 100)
 })
