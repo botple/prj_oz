c7474fec5886c137a64a1a2696dccb5675ec9f21
botple <haminseung1st@gmail.com>
2026-08-28 01:23:19 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,9 +1,5 @@
 DFRobotMaqueenPlus.I2CInit()
 
 basic.forever(function () {
-    let distance = DFRobotMaqueenPlus.ultraSonic(PIN.P0, PIN.P0)
-
-    basic.showNumber(distance)
-
-    basic.pause(500)
+    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 100)
 })
