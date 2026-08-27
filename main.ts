42302cf9756172824064b573e92795869586924d
botple <haminseung1st@gmail.com>
2026-08-27 23:26:22 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
-})
+DFRobotMaqueenPlus.I2CInit()
+
+DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
+
+basic.pause(2000)
+
+DFRobotMaqueenPlus.mototStop(Motors.ALL)
