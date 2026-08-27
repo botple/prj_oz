4bfd4efa474b02b12758ae1b8f6908f319063af9
botple <haminseung1st@gmail.com>
2026-08-28 01:46:40 +0900

    main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,6 +1,30 @@
+let running = false
+let turnRight = false
+
 DFRobotMaqueenPlus.I2CInit()
 
+input.onButtonPressed(Button.A, function () {
+    running = !running
+})
+
+input.onButtonPressed(Button.B, function () {
+    turnRight = !turnRight
+})
+
 basic.forever(function () {
-    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 100)
-    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 100)
+    if (!running) {
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    } else {
+        if (turnRight) {
+            // 우회전
+            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 110)
+            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
+        } else {
+            // 좌회전
+            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 50)
+            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 110)
+        }
+    }
+
+    basic.pause(50)
 })
